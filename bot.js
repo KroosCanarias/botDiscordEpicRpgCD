9df895867b299dfc447e368fa82d44dad4857b07
KroosCanarias <ortiz0kevin0falcon@gmail.com>
2020-07-21 22:44:53 +0100

    Base code

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const  client = new Discord.Client();
+const array=["rpg chop","rpg axe","rpg bowsaw","rpg chainsaw",
+"rpg fish","rpg net","rpg boat","rpg bigboat",
+"rpg mine","rpg pickaxe","rpg drill","rpg dynamite",
+"rpg pickup","rpg ladder","rpg tractor","rpg greenhouse"];
+client.on("ready", () => {
+   console.log("Estoy listo!");
+});
+client.on("message", (message) => {
+    const args = message.content.trim();
+    const command = args.toLowerCase();
+    if (command=="rpg hunt") {
+        setTimeout(function() {
+  		const channel = message.channel;
+        	channel.send(message.author.toString() + " rpg hunt ready");
+	    }, 60000);
+    }
+    if (array.includes(command)) {
+        setTimeout(function() {
+  		const channel = message.channel;
+        	channel.send(message.author.toString() + " "+command+" ready");
+	    }, 300000);
+    }
+    if (command=="rpg tr" || command=="rpg training") {
+        setTimeout(function() {
+  		const channel = message.channel;
+        	channel.send(message.author.toString() + " rpg training ready");
+	    }, 1500000);
+    }
+
+});
+client.login("NzE2NjkwMzY1OTUxOTAxNzY2.XxWkCA.JYL4qeL06At8L_F9VmBVmMmPxiw"); 
